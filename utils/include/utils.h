9066facd7cdbb1fc81e97f7e858a342c9322cf2a
TanishqChaudhary <tanishqchaudhary101010@gmail.com>
2025-01-26 20:56:07 +0530

    update the method to access robot parameter

--- utils/include/utils.h
+++ utils/include/utils.h
@@ -7,15 +7,20 @@ namespace utils {
 
 class Robot {
  public:
-  int id;
-  float battery;
-  bool charging_status;
-
   Robot() : id(0), battery(0), charging_status(0) {}
   Robot(int id, float battery, bool charging_status)
       : id(id), battery(battery), charging_status(charging_status) {}
 
   ~Robot() = default;
+
+  inline int getRobotId() const { return id; }
+  inline float getRobotBattery() const { return battery; }
+  inline float getChargingStatus() const { return charging_status; }
+
+ private:
+  int id = 0;
+  float battery = 0;
+  bool charging_status = 0;
 };
 
 void printRobotBatteries(std::vector<utils::Robot>& robots);
