885961ba3d7e16c401ea911e8ca0924c6c2fce60
TanishqChaudhary <tanishqchaudhary101010@gmail.com>
2025-01-26 20:23:56 +0530

    added executable for running RobotBMS

---
+++ src/run_robot_bms.cpp
@@ -0,0 +1,35 @@
+#include <signal.h>
+
+#include <bms.cpp>
+
+bool flg_run = true;
+void signal_callback_handler(int signal) {
+  flg_run = false;
+  if (signal == SIGINT)
+    std::cout << "[main] Exiting on SIGINT\n";
+  else {
+    std::cout << "[main] Exiting on Abort or Seg-Fault\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+}
+
+int main() {
+  signal(SIGINT, signal_callback_handler);
+
+  std::vector<utils::Robot> robots;
+  robots.emplace_back(utils::Robot(1, 10, false));
+  robots.emplace_back(utils::Robot(2, 20, false));
+  robots.emplace_back(utils::Robot(3, 20, false));
+  robots.emplace_back(utils::Robot(4, 40, false));
+  robots.emplace_back(utils::Robot(5, 40, false));
+
+  bms::BMS robotBMS(1.5, 1, 2);
+
+  while (flg_run) {
+    utils::printRobotBatteries(robots);
+    robotBMS.run(robots);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+
+  return 0;
+}
