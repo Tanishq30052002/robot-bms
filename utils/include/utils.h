3ae1a39366c4b2e1d1d332bd73b80b8c4d01b58a
TanishqChaudhary <tanishqchaudhary101010@gmail.com>
2025-01-28 23:19:34 +0530

    added doxygen comments on utils

--- utils/include/utils.h
+++ utils/include/utils.h
@@ -7,17 +7,62 @@ namespace utils {
 
 class Robot {
  public:
+  /**
+   * @brief Construct a new Robot object with default values
+   *
+   */
   Robot() : id_(0), battery_(0), charging_status_(0) {}
+
+  /**
+   * @brief Construct a new Robot object based on the values
+   *
+   * @param id
+   * @param battery
+   * @param charging_status
+   */
   Robot(int id, float battery, bool charging_status)
       : id_(id), battery_(battery), charging_status_(charging_status) {}
 
+  /**
+   * @brief Destroy the Robot object
+   *
+   */
   ~Robot() = default;
 
+  /**
+   * @brief Get the Robot Id object
+   *
+   * @return int
+   */
   inline int getRobotId() const { return id_; }
+
+  /**
+   * @brief Get the Robot Battery object
+   *
+   * @return float
+   */
   inline float getRobotBattery() const { return battery_; }
+
+  /**
+   * @brief Get the Charging Status object
+   *
+   * @return true
+   * @return false
+   */
   inline bool getChargingStatus() const { return charging_status_; }
 
+  /**
+   * @brief Set the Robot Battery object
+   *
+   * @param robot_battery
+   */
   inline void setRobotBattery(float robot_battery) { battery_ = robot_battery; }
+
+  /**
+   * @brief Set the Charging Status object
+   *
+   * @param robot_charging_status
+   */
   inline void setChargingStatus(bool robot_charging_status) {
     charging_status_ = robot_charging_status;
   }
@@ -28,5 +73,13 @@ class Robot {
   bool charging_status_ = 0;
 };
 
+/**
+ * @brief prints the robot batteries
+ *
+ * Step 1: It sorts the robots based on the id
+ * Step 2: It prints the robot id and battery level
+ *
+ * @param robots
+ */
 void printRobotBatteries(std::vector<utils::Robot>& robots);
 }  // namespace utils
